23caa4077b7b5574649471bd8728e5144857063c
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-12 11:18:01 -0300

    :gift: Alert Ok

--- swift-004/ViewController.swift
+++ swift-004/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var btn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func mostrarAlerta(_ sender: UIButton){
+        let alerta = UIAlertController(title: "Nao primeiro App", message: "Funfou lek 🥳", preferredStyle: UIAlertController.Style.alert)
+        alerta.addAction(UIAlertAction(title: "Deu bom", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alerta, animated: true, completion: nil)
     }
 
 
